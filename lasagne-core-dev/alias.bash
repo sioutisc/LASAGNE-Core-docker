bd85d81bc66961c0d448ee001091ecd8b4c373de
Christos Sioutis <christos.sioutis@gmail.com>
2018-05-25 22:58:47 +0930

    no longer needs to prefix DEV_CMD with -c

--- lasagne-core-dev/alias.bash
+++ lasagne-core-dev/alias.bash
@@ -4,21 +4,21 @@ Lshell() {
 }
 
 Lmwc() {
-  CMD="-c \$ACE_ROOT/bin/mwc.pl $@"
+  CMD="\$ACE_ROOT/bin/mwc.pl $@"
   docker run -e "DEV_CMD=$CMD" -v $(pwd):/opt/DEV --rm -it lasagne-core-dev
 }
 
 Lmake() {
-  CMD="-c make $@"
+  CMD="make $@"
   docker run -e "DEV_CMD=$CMD" -v $(pwd):/opt/DEV --rm -it lasagne-core-dev
 }
 
 LTAFServer() {
-  CMD="-c \$DAF_ROOT/bin/TAFServer $@"
+  CMD="\$DAF_ROOT/bin/TAFServer $@"
   docker run -e "DEV_CMD=$CMD" -v $(pwd):/opt/DEV --rm -it lasagne-core-dev
 }
 
 Lmdu() {
-  CMD="-c MulticastDiscoveryUtility"
+  CMD="MulticastDiscoveryUtility"
   docker run -e "DEV_CMD=$CMD" -v $(pwd):/opt/DEV --rm -it lasagne-core-dev
 }
